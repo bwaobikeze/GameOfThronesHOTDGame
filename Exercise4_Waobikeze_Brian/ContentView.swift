f924afe5d6eaa705952211680c5d07e340ff6923
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-09-15 03:42:43 -0500

    Initial Commit

---
+++ Exercise4_Waobikeze_Brian/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Exercise4_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Exercise4_Waobikeze_Brian/Exercise4_Waobikeze_BrianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise4_Waobikeze_BrianApp.swift
+//  Exercise4_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise4_Waobikeze_BrianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
